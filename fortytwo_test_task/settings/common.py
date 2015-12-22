b5fe4ea2b5adb700eda4d0181aa2ce2de4d093b7
Ziberon <zironni@github.com>
2015-12-22 03:42:03 +0200

    first commit

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -24,7 +24,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'x=c0_e(onjn^80irdy2c221#)2t^qi&6yrc$31i(&ti*_jf3l8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
